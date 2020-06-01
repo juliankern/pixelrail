4f2b89c63f90b668db33a56b1f42a4b5d0141a75
Julian Kern <mail@juliankern.com>
2020-06-01 17:06:39 +0200

    added current arduino program

--- serialread.ino
+++ serialread.ino
@@ -1,78 +1,84 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
 // On a Trinket or Gemma we suggest changing this to 1:
-#define LED_PIN 6
+#define LED_PIN    6
 
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT 12
+#define LED_COUNT 150
 
 // Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(12, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-#define CHUNK_SIZE 45
-unsigned char buffer[45];
-int buffer2;
-int ledCounter = 1;
+#define CHUNK_SIZE 15
+
+#define SIGNAL_READY 1
+
+int ledCounter = 0;
 int chunkCounter = 1;
+//int bufferCounter = 0;
+unsigned char buffer[3];
 
-void setup()
-{
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
-
-  strip.begin();            // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();             // Turn OFF all pixels ASAP
-  strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
-
-  Serial.begin(36000);
-  Serial.println('1');
+void sendReady() {
+  //Serial.flush();
+  Serial.println();
+  Serial.println(SIGNAL_READY);
 }
 
-void loop()
-{
-  while (Serial.available() < CHUNK_SIZE)
-  {
-  }
-  buffer2 = Serial.read();
-  Serial.print(buffer[0]);
-  /*if (Serial.available())  {
-    int messageLength = Serial.readBytes(buffer, 3);
+void setup() {
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
 
-    if (messageLength == 3) {
-      Serial.print("LED:");
-      Serial.print(ledCounter);
-      Serial.print("-RGB:");
-      Serial.print(buffer[0]);
-      Serial.print(",");
-      Serial.print(buffer[1]);
-      Serial.print(",");
-      Serial.print(buffer[2]);
+  
+  Serial.begin(2000000);
+  sendReady();
+}
 
+void loop() { 
+  if (Serial.available())  {
+    /*Serial.println("got values?");
+    Serial.print(Serial.available());*/
+  
+    if (Serial.readBytes(buffer, 3) == 3) {
+      /*if (buffer[0] != 0) {
+        switch (buffer[0]) {
+          case 1:
+            // Start command
+            ledCounter = 0;
+            chunkCounter = 1;
+            break; 
+        }
+      }*/
+      
+      
+      
       strip.setPixelColor(ledCounter, strip.Color(buffer[0],buffer[1],buffer[2]));
-      strip.show();
+      //strip.setPixelColor(buffer[0], strip.Color(buffer[1],buffer[2],buffer[3]));
+      //strip.show();
       
       ledCounter++;
 
-      if (ledCounter > 150) {
-        ledCounter = 1;
+      /*Serial.print(ledCounter);
+      Serial.print(">");
+      Serial.print(chunkCounter);
+      Serial.print("*");
+      Serial.print(CHUNK_SIZE);
+      Serial.print("=");
+      Serial.print(ledCounter > chunkCounter * CHUNK_SIZE);*/
+      if (ledCounter == chunkCounter * CHUNK_SIZE) {
+        chunkCounter++;
+        sendReady();
+      }
+
+      if (ledCounter == LED_COUNT) {
+        strip.show();
+        ledCounter = 0;
+        chunkCounter = 1;
       }
     } else {
       Serial.println("ERROR, did not receive enough values");
+      //Serial.print(Serial.readBytes(buffer, 3));
     }
-    
   }
-
-  /*for(int i = 0; i < sizeof(values); i++) {
-    Serial.println(values[i]);
-  }*/
-
-  delay(100);
 }
