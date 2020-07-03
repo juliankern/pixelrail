aad232a8c17ea23ee5690a4edcf423113eb7bbe0
Julian Kern <mail@juliankern.com>
2020-07-03 14:35:29 +0200

    switch from dmxnet to kt-artnet

--- artnet-test.js
+++ artnet-test.js
@@ -1,16 +1,19 @@
 // Load dmxnet as libary
-var dmxlib = require('dmxnet');
-// Create new dmxnet instance
-var dmxnet = new dmxlib.dmxnet({});
+const Artnet = require('kt-artnet');
+var artnet = new Artnet({});
+//console.log(artnet.interfaces);
 
 // Create a new receiver instance, listening for universe 5 on net 0 subnet 0
-var receiver = dmxnet.newReceiver({
+var receiver = artnet.createReceiver({
     subnet: 0,
-    universe: 5,
+    universe: 0,
     net: 0,
 });
 
+const toHex = val => val.toString(16)
+
 // Dump data if DMX Data is received
 receiver.on('data', function (data) {
-    console.log('DMX data:', data); // eslint-disable-line no-console
+    const formatted = data.map(toHex).slice(0, 10).join(',')
+    console.log('DMX data:', formatted); // eslint-disable-line no-console
 });
