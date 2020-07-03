cb76252ee05fb63ef628469bc122fe06e1fdb251
Julian Kern <mail@juliankern.com>
2020-07-03 14:49:18 +0200

    switch from kt-artnet to artnode

--- artnet-test.js
+++ artnet-test.js
@@ -1,19 +1,17 @@
-// Load dmxnet as libary
-const Artnet = require('kt-artnet');
-var artnet = new Artnet({});
-//console.log(artnet.interfaces);
+const { ArtNet } = require('artnode');
 
-// Create a new receiver instance, listening for universe 5 on net 0 subnet 0
-var receiver = artnet.createReceiver({
-    subnet: 0,
-    universe: 0,
-    net: 0,
+const artnet = new ArtNet({ isController: true });
+
+const universe = artnet.getUniverse(0);
+
+universe.on('data', ({ data, changed }) => {
+    changed.forEach(({ address, value }) => {
+        console.log(`DMX ${address} set to ${value}.`);
+    });
+
+    data.forEach((value, address) => {
+        console.log(`DMX ${address} is ${value}`);
+    });
 });
 
-const toHex = val => val.toString(16)
-
-// Dump data if DMX Data is received
-receiver.on('data', function (data) {
-    const formatted = data.map(toHex).slice(0, 10).join(',')
-    console.log('DMX data:', formatted); // eslint-disable-line no-console
-});
+artnet.start();
