7a968ae59514716969e8b86ae51635621d5b29fe
Julian Kern <mail@juliankern.com>
2020-07-03 14:18:01 +0200

    various changes

--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,31 @@
 const Datastream = require('../lib/Datastream.class');
+const SerialPort = require('serialport');
+// const { init } = require('hap-nodejs');
 
-const LED_COUNT = 150;
+const LED_COUNT = 300;
 const UPDATE_FPS = 30;
+
 const dataPackage = Array.from({ length: LED_COUNT }, e => Array(3).fill(0));
+
 // const dataPackage = Array.from({ length: LED_COUNT }, e => Array(4).fill(0));
 // dataPackage.forEach((arr, i) => arr[0] = i);
 
 if (process.argv[2] === 'test') {
     console.log(`TEST mode activated, sending random LED values at ${UPDATE_FPS}fps`);
 } else if (process.argv[2] === 'list') {
-    (async () => {
-        const SerialPort = require('serialport');
+    return (async () => {
         console.log(await SerialPort.list());
     })();
 } else {
-    const HomeKitReceiver = require('../lib/HomeKitReceiver.component');
-    const ArtNetReceiver = require('../lib/ArtNetReceiver.component');
+    const HomeKitAdapter = require('../lib/Adapters/HomeKit');
+    const ArtNetAdapter = require('../lib/Adapters/ArtNet');
+    const WebsocketAdapter = require('../lib/Adapters/Websocket');
 
-    const hkReceiver = new HomeKitReceiver();
-    const anReceiver = new ArtNetReceiver();
+    const hkAdapter = new HomeKitAdapter();
+    const anAdapter = new ArtNetAdapter();
+    const wsAdapter = new WebsocketAdapter();
 
-    hkReceiver.on('data', ({ pixel, r, g, b }) => {
+    hkAdapter.on('data', ({ pixel, r, g, b }) => {
         console.log('updatePixel triggered', pixel, r, g, b);
         pixel.forEach(i => {
             const oldRGB = [
@@ -31,26 +36,63 @@ if (process.argv[2] === 'test') {
             // dataPackage[i] = [r,g,b];
             colorFade(oldRGB, [r, g, b], 1000, ({ r, g, b }) => {
                 // console.log('color fade', r,g,b);
-                dataPackage[i] = [
-                    // i, 
-                    r, g, b
-                ];
+                dataPackage[i] = [r, g, b];
             });
         });
     });
 
-    anReceiver.on('data', (data) => {
-        dataPackage = [...data];
+    anAdapter.on('data', (data) => {
+        dataPackage.map((d, i) => data[i]);
     });
-    
+
+    wsAdapter.on('data', (data) => {
+        dataPackage.map((d, i) => data[i]);
+    });
+
+    wsAdapter.on('data-fade', (data) => {
+        const oldData = [...dataPackage];
+
+        dataPackage.forEach((pixel, i) => {
+            colorFade(oldData[i], data[i], 1000, ({ r, g, b }) => {
+                // console.log('color fade', i, [r,g,b]);
+                dataPackage[i] = [r, g, b];
+            });
+        });
+    });
 }
 
-const stream = new Datastream();
-stream.on(Datastream.EVENTS.CONNECTION_OPEN, _ => {
-    console.log('open! sending data...');
-    sendDatastream();
-    stream.start();
-})
+let useablePorts = [];
+
+(async () => {
+    const availablePorts = await SerialPort.list();
+    useablePorts = availablePorts.filter(_ => _.vendorId === '1a86');
+    useablePorts.sort((a, b) => +a.locationId - +b.locationId);
+
+    init();
+})();
+
+
+function init() {
+    let openPorts = 0;
+
+    useablePorts.forEach(port => {
+        port.stream = new Datastream(port.path);
+        
+        port.stream.on(Datastream.EVENTS.CONNECTION_OPEN, _ => {
+            console.log('open! sending data...');
+            openPorts++;
+            // port.stream.start();
+            portOpened();
+        });
+    });
+
+    function portOpened() {
+        if (openPorts === useablePorts.length) {
+            sendDatastream();
+            useablePorts.forEach(_ => _.stream.start());
+        }
+    }
+}
 
 async function sendDatastream() {
     if (process.argv[2] === 'test') {
@@ -64,7 +106,11 @@ async function sendDatastream() {
         }
     }
 
-    stream.setData(dataPackage);
+    const splitData = splitInChunks(dataPackage, useablePorts.length);
+
+    useablePorts.forEach((port, i) => {
+        port.stream.setData(splitData[i]);
+    });
 
     starttime = Date.now();
 
@@ -72,7 +118,6 @@ async function sendDatastream() {
         sendDatastream();
     }, 1000 / UPDATE_FPS);
 }
-
 
 async function sleep(time) {
     return new Promise((resolve) => {
@@ -103,4 +148,11 @@ function colorFade(start, end, duration, callback) {
 
 function lerp(a, b, u) {
     return (1 - u) * a + u * b;
+}
+
+function splitInChunks(arr, chunkSize) {
+    var R = [];
+    for (var i = 0, len = arr.length; i < len; i += chunkSize)
+        R.push(arr.slice(i, i + chunkSize));
+    return R;
 }
